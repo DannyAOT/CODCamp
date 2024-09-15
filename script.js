246436215261ea1cc75106cb06ff39320a858bf7
DannyAOT <danielajibade16@gmail.com>
2024-09-15 16:31:05 +0100

    script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ window.onscroll = () => {
 
         if(top => offset && top < offset + height) {
             navLinks.forEach(links => {
-                links.classList.remove('');
+                links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id +']').classList.add('active');
             });
         };
@@ -32,8 +32,3 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 };
 
-var loader = document.getElementById(preload)
-    
-window.addEventListener("load", function(){
-    loader.style.display = "none";
-})
